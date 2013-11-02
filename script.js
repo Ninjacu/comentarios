d6f62995ed04659978d002ef06669d6af24cd7de
Ninjacu <Call11910>
2013-11-02 16:42:39 +0100

    Hola

--- script.js
+++ script.js
@@ -6,9 +6,13 @@ $(document).ready(function() {
     $("#enviar").on('click',function(){
         var texto=$("#comentarios").val();
         var fecha= new Date();
+        var cruz= "<img class='eliminar' src='error.png'>"+"</img>";
         var fechaComentario= "<span class='fecha'>"+fecha.getHours()+":"+fecha.getMinutes()+":"+fecha.getSeconds()+"</span>";
         var divComentarioEscrito= "<div class='comentario-enviado'>";
-        var divCompleto= divComentarioEscrito+fechaComentario+"   "+texto+"</div>";
+        var divCompleto= divComentarioEscrito+fechaComentario+"   "+texto+"   "+cruz+"</div>";
             $("#comentarios-escritos").prepend(divCompleto);
+    });
+    $("#comentarios-escritos").on('click', '.eliminar', function(){
+       alert("ELIMINAR");
     });
 });
